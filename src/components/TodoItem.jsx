28792ec62dee7ad79e30a06be81c7e46d2ecc4d0
Prakarshprasar <prakprasar@gmail.com>
2022-05-21 23:53:44 +0530

    changes

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import style from "./todo.module.css"
 
 
+
 const TodoItem = ({arr}) => {
-    const [borcol,setBorcol]=useState("7px solid teal")
+  
   return (
     <div className={style.tododiv}>
         {arr.map((todos)=>(
-            <p className={style.lborder}><span style={{border:borcol}} onClick={()=>{
-                setBorcol("7px solid orangered")
-            }}></span>{todos.value}</p> 
+            <div className={style.lborder}>
+              <input type="checkbox" className={style.chinp} />
+              <label for="checkbox">{todos.value}</label>
+            </div> 
         ))}
     </div>
   )
